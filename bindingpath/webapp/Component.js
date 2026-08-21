2e7ff738a3edda8e914dad533562cab2bd30b27d
Gustavo Brito <gustavo.ponciano2005@gmail.com>
2026-08-21 22:15:42 +0000

    centralizing configurations in the manifest.json file

--- bindingpath/webapp/Component.js
+++ bindingpath/webapp/Component.js
@@ -5,46 +5,7 @@ sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/mvc/XMLView",
         return UIComponent.extend("ui5.walkthrough.Component", {
 
             "metadata": {
-                "rootView": "ui5.walkthrough.view.App",
-                "config": {
-                    "serviceUrl": "model/defaultModel.json"
-                },
-                "routing": {
-                    "config": {
-                        "routerClass": "sap.m.routing.Router",
-                        "type": "View",
-                        "viewType": "XML",
-                        "path": "ui5.walkthrough.view",
-                        "controlId": "app",
-                        "controlAggregation": "pages",
-                        "transition": "slide",
-                        "async": true
-                    },
-                    "routes": [{
-                        "pattern": "",
-                        "name": "master",
-                        "target": "master"
-                    },{
-                        "pattern": "detail/{ID}",
-                        "name": "detail",
-                        "target": "detail"
-                    }],
-                    "targets": {
-                        "master": {
-                            "id": "master",
-                            "name": "First",
-                            "path": "ui5.walkthrough.view",
-                            "level": 1
-                        },
-                        "detail": {
-                            "id": "detail",
-                            "name": "Second",
-                            "path": "ui5.walkthrough.view",
-                            "level": 2
-                        }
-                    }
-                }
-
+                "manifest": "json",
             },
 
             init: function(){
@@ -53,30 +14,7 @@ sap.ui.define(["sap/ui/core/UIComponent", "sap/ui/core/mvc/XMLView",
                 this.getRouter().initialize()
             },
 
-            createContent: function () {
-                var oView = UIComponent.prototype.createContent.apply(this, arguments)
-
-                var sPath = this.getMetadata().getConfig().serviceUrl
-
-                var oModel = new JSONModel(sPath)
-                this.setModel(oModel)
-
-                this.setModel(new ResourceModel({
-                    bundleName: "ui5.walkthrough.i18n.i18n"
-                }), "i18n")
-
-                // XMLView.create({
-                //     viewName: "ui5.walkthrough.view.App"
-                // }).then(
-                //     (oView) => {
-                //         oView.placeAt("content")
-                //     }
-                // )
-
-                oView.placeAt("content")
-
-                return oView
-            }
+            
         });
     }
 );
